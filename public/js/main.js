01ab4be80d6b1c4dcf2677e15dde5b8720f30ad5
Abhishek kumar pathak <rajkumar25mth@gmail.com>
2024-03-08 03:18:22 +0530

    nodejs project first commit

---
+++ public/js/main.js
@@ -0,0 +1,12 @@
+// const cityName = document.getElementById('cityName');
+const submitBtn = document.getElementById('submitBtn');
+// const city_name = document.getElementById('city_name');
+
+
+const getInfo = (event) =>{
+    event.preventDefault();
+    alert(hii);
+
+    // let url =`https://v1.nocodeapi.com/pathak123/ow/QfzBijnNJlRQcTCH/byCityName?q=london`
+}
+submitBtn.addEventListener('click', getInfo);
